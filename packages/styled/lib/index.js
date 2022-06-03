ba68944998c45b7aeec373d2ccbea39895eb1057
jian.gao <jian.gao@shopee.com>
2022-06-03 09:08:27 +0800

    feat: new version

--- packages/styled/lib/index.js
+++ packages/styled/lib/index.js
@@ -1,5 +1,5 @@
 import { createCss, createStyled } from "./primitives-core/index.js";
-import StyleSheet from "@styled-box/react-primitives/lib/StyleSheet.js";
+import StyleSheet from "@styled-box/react-primitives/lib/StyleSheet/StyleSheet.js";
 
 export const css = createCss(StyleSheet);
 export const styled = createStyled(StyleSheet);
