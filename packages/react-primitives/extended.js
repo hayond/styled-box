7309cdeac495fc1fa3aa6f27b83b0f6bfbe2ef14
jian.gao <jian.gao@shopee.com>
2022-06-07 12:48:55 +0800

    feat: add exports extended entry

---
+++ packages/react-primitives/extended.js
@@ -0,0 +1,4 @@
+export { default as StyleSheet } from "./lib/exports/StyleSheet/index.js";
+export { default as View } from "./lib/exports/View/index.js";
+export { default as Text } from "./lib/exports/Text/index.jsx";
+export { default as Image } from "./lib/exports/Image/index.jsx";
